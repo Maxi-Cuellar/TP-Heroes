6087ae26da0f2c51cbd034c10944974b9bccf2f9
maxi.cuellar.2002@gmail.com <maxi.cuellar.2002@gamil.com>
2022-11-28 11:46:50 -0300

    Agregue info

---
+++ app.js
@@ -0,0 +1,19 @@
+const express =  require ("express")
+const app = express()
+const puerto = 3030 
+const path = require ("path")
+
+app.use(express.static("public"))
+
+app.get("/",(req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
+app.get("/babbage.html",(req, res) => res.sendFile(path.join(__dirname, "views", "babbage.html")))
+app.get("/berners-lee.html",(req, res) => res.sendFile(path.join(__dirname, "views", "berners-lee.html")))
+app.get("/clarke.html",(req, res) => res.sendFile(path.join(__dirname, "views", "clarke.html")))
+app.get("/hamilton.html",(req, res) => res.sendFile(path.join(__dirname, "views", "hamilton.html")))
+app.get("/hopper.html",(req, res) => res.sendFile(path.join(__dirname, "views", "hopper.html")))
+app.get("/lovelace.html",(req, res) => res.sendFile(path.join(__dirname, "views", "lovelace.html")))
+app.get("/turing.html",(req, res) => res.sendFile(path.join(__dirname, "views", "turing.html")))
+app.get("*",(req, res) => res.sendFile(path.join(__dirname, "views", "404.html")))
+
+
+app.listen(puerto, () => console.log(`Servidor funcionando en http://localhost:${puerto}`))
